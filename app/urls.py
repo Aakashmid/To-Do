08cf83e5b2876f74c52823cb0bce1270bc9049e4
Aakash Kumar Jha <aakash13102005u@gmail.com>
2023-12-25 21:02:54 +0530

    modified index.html

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .import views
   
 urlpatterns = [ 
- 
     path('', views.home, name='Home') ,
     path('addTask', views.add_Task, name='Add task') ,
     path('deleteTask', views.delete_Task, name='Deleting  task') ,
